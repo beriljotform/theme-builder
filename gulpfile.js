01f26b24a4c2e650b5d15181aec52a74e6879a31
beriljotform <beril@jotform.com>
2023-03-01 15:16:48 +0300

    changes

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,12 @@ const { src, dest, watch, task } = require("gulp");
 
 const sass = require("gulp-sass");
 const postcss = require("gulp-postcss");
-const autoprefixer = require("autoprefixer");
-const cssnano = require("cssnano");
 const cssImport = require('gulp-cssimport');
 
 const createThemeJob = function() {
   return src("src/*.scss")
     .pipe(sass())
     .pipe(cssImport())
-    .pipe(postcss([autoprefixer(), cssnano()]))
     .pipe(dest("dist"));
 }
 
